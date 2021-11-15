c5c87c649fe0998429abce9a2ace5a9164f2e7da
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2021-11-15 17:42:15 -0300

    lerp function

--- main.cpp
+++ main.cpp
@@ -25,9 +25,23 @@ Vector3f Reflection(Vector3f& a, Vector3f& n)
     float Prod = GetDotProduct(TempA,n);
     return {2.0f*(Prod*n+a)};
 }
+// Lerp
+Vector2f FLerp(Vector2f PosA,Vector2f PosB,float Time)
+{
+    return PosA + (PosB - PosA) * Time;
+}
 
 int main()
 {
+    Vector2f a = {-4,3};
+    Vector2f b = {8,6};
+    Vector2f lerpA = FLerp(a,b,0.25);
+    std::cout << "A 0.25 "<< lerpA.x << ", " << lerpA.y << std::endl;
+    Vector2f lerpB = FLerp(a,b,0.5);
+    std::cout << "A 0.5 "<< lerpB.x << ", " << lerpB.y << std::endl;
+    Vector2f lerpC = FLerp(a,b,0.75);
+    std::cout << "A 0.75 "<< lerpC.x << ", " << lerpC.y << std::endl;
+/**
     Vector3f a={-0.71f,0.0f,0.71f};
     Vector3f n={1,0,0};
     Vector3f b={1,1,3};
@@ -36,6 +50,6 @@ int main()
     Vector3f proA ={-0.6,0,-0.8};
     Vector3f NegProA = {-proA.x,-proA.y,-proA.z};
     Vector3f proN ={0.71,0,0.71};
-
+*/
     return 0;
 }
